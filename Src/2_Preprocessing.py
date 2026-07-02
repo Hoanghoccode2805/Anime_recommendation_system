b311716aef6f9b460f833a048b86dc86d3e23232
Hoanghoccode2805 <hoangbuinhat05@gmail.com>
2026-07-02 06:25:41 +0200

    Add some explore data with ipybb

--- Src/2_Preprocessing.py
+++ Src/2_Preprocessing.py
@@ -1,19 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 def data_cleaning():
     address = r"D:\Full projet\Anime_recommendation_system\Data\my_anime_raw_dataset.csv"
     df = pd.read_csv(address)
     
-    # View data
-    print(df.head())
-    print(df.info())
-    non_null_percentage = df.notna().mean() * 100
-    print(non_null_percentage)
-    print(df.describe())
+    
 
-    df = df.drop_duplicates()
-    print("=== After drop duplicates ===")
-    non_null_percentage = df.notna().mean() * 100
-    print(non_null_percentage)
 data_cleaning()
